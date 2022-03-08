a6005103e7c9822960a96e3c01411c076eee8c1b
Joao Baumotte <jbaumotte@gmail.com>
2022-03-08 16:34:01 -0300

    Update - Textos principal

--- Components/Texto.js
+++ Components/Texto.js
@@ -8,16 +8,25 @@ export default function Texto() {
           <Titulo />
 
           <p>
-            Gente vamos fazer esse chá de fralda da Andressa e do Luis Felipe
-            bombar! Com duas eles vão precisar de muita fralda!{' '}
+            Gente, estamos organizando um chá de fraldas SURPRESA para as gêmeas
+            da Andressa e do Luis Felipe!
           </p>
           <p>
-            Como não sabemos onde eles vão morar e se tem espaço, vamos fazer
-            uma vaquinha para comprar fraldas.
+            Vamos fazer esse chá de fraldas bombar! Com duas eles vão precisar
+            de muitas fraldas!!
           </p>
           <p>
-            Aproveita e deixa uma mensagem pra eles que vamos fazer uma edição
-            bem legal! Depois a gente manda a reação deles pra vocês
+            Como não sabemos onde eles vão morar e se terão espaço para guardar
+            as fraldas, vamos fazer uma vaquinha para eles irem comprando as
+            fraldas conforme forem precisando.
+          </p>
+          <p>
+            Aproveita e deixa uma mensagem pra eles que iremos fazer uma edição
+            bem legal! Depois a gente manda a reação deles pra vocês!
+          </p>
+          <p>
+            Contamos com ajuda até o dia 10/04/2022 para termos tempo de
+            organizar tudo antes das Marias nascerem!
           </p>
         </div>
       </div>
